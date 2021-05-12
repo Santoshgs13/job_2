a2b137bf3988fe774b33312c1462a2baf8df1e7a
Santoshgs13 <81846783+Santoshgs13@users.noreply.github.com>
2021-05-12 12:35:08 +0530

    Create SubTwoNumbers.java

---
+++ SubTwoNumbers.java
@@ -0,0 +1,10 @@
+public class SubTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, diff;
+      sum = num2 - num1;
+
+      System.out.println("Diff of these numbers: " +diff);
+   }
+}
